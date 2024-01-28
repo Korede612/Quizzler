8c3ccec658190acb66437c17c09b44f9071a7218
Korede612 <korede.oko-osi@nytengaletech.com>
2024-01-28 13:11:13 +0100

    Initial Commit

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Oko-osi Korede on 28/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
